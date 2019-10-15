63a2517ea7f06208d22479f61bc8166dcbb5933e
Thamina33 <37588746+Thamina33@users.noreply.github.com>
2019-10-16 01:21:58 +0600

    x

---
+++ app/src/main/java/com/example/bloodhero/allDonorList.java
@@ -0,0 +1,14 @@
+package com.example.bloodhero;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class allDonorList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_donor_list);
+    }
+}
