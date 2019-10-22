598654884caae70226c491a54e13b1839c469e0b
rahat14 <rahatshovo6666@gmail.com>
2019-10-22 13:18:23 +0600

    gg
    gg

--- app/src/main/java/com/example/bloodhero/create_Profile.java
+++ app/src/main/java/com/example/bloodhero/create_Profile.java
@@ -4,12 +4,18 @@ package com.example.bloodhero;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 
+import android.Manifest;
 import android.app.Dialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -26,16 +32,31 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+import id.zelory.compressor.Compressor;
 
 public class create_Profile extends AppCompatActivity {
 
     EditText dname, dEmail;
+    Uri mFilePathUri ;
+    private Bitmap compressedImageFile;
     LinearLayout gmale, gfemale;
     ImageView m_icon, f_icon;
     TextView m_txt, f_txt;
@@ -49,6 +70,7 @@ public class create_Profile extends AppCompatActivity {
     DatabaseReference mRef;
     ProgressBar mbar;
     FirebaseAuth mauth ;
+    StorageReference mStorageReference ;
     String uid ;
     CircleImageView imageView ;
 
@@ -60,7 +82,7 @@ public class create_Profile extends AppCompatActivity {
         setContentView(R.layout.activity_create__profile);
 
         mRef = FirebaseDatabase.getInstance().getReference("DonorInformation");
-
+        mStorageReference = FirebaseStorage.getInstance().getReference("profilePic") ;
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
@@ -438,10 +460,171 @@ public class create_Profile extends AppCompatActivity {
             }
         });
 
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+                    if (ContextCompat.checkSelfPermission(create_Profile.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+                        ActivityCompat.requestPermissions(create_Profile.this,
+                                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+
+                        BringImagePicker();
+
+
+
+                    } else {
+
+                        BringImagePicker();
+
+                    }
+
+                } else {
+
+                    BringImagePicker();
+
+                }
+
+
+            }
+        });
+
+
+
+    }
+    @Override
+    protected void onActivityResult(
+            int requestCode, int resultCode, Intent data) {
+        //  super.onActivityResult(requestCode, resultCode, data);
+
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+
+                mFilePathUri = result.getUri();
+                imageView.setImageURI(mFilePathUri);
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+
+                Exception error = result.getError();
+                Toast.makeText(this, error.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        }
+
     }
 
-    public void uploadDataToFireBase(String name, String email, String gndr, String bldGroup, String numVisibility) {
+    private void BringImagePicker() {
 
+        CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .setAspectRatio(1,1)
+                .setCropShape(CropImageView.CropShape.OVAL) //shaping the image
+                .start(create_Profile.this);
+
+
+    }
+
+    public void uploadDataToFireBase(final String name, final String email, final String gndr, final String bldGroup, final String numVisibility) {
+
+
+        if(mFilePathUri != null)
+        {
+
+            final String randomName = UUID.randomUUID().toString();
+
+            // PHOTO UPLOAD
+            File newImageFile = new File(mFilePathUri.getPath());
+
+            try {
+
+                compressedImageFile = new Compressor(create_Profile.this)
+                        .setMaxHeight(920)
+                        .setMaxWidth(920)
+                        .setQuality(40)
+                        .compressToBitmap(newImageFile);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            compressedImageFile.compress(Bitmap.CompressFormat.JPEG, 40, baos);
+            byte[] imageData = baos.toByteArray();
+            UploadTask filePath = mStorageReference.child(randomName+uid + ".jpg").putBytes(imageData);
+            filePath.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+
+                   // viewDialog.hideDialog();
+
+                    sentToHome();
+                  //  viewDialog.hideDialog();
+
+                    Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
+                    while (!uriTask.isSuccessful()) ;
+                    Uri downloaduri = uriTask.getResult();
+
+
+
+                    //  String ts =mDatabaseReference.push().getKey() ;
+                    String id = mRef.push().getKey();
+
+                    getProfile model = new getProfile(id, uid, name, email, gndr, bldGroup, numVisibility , "TRUE" , downloaduri.toString());
+
+                    //   String imageUploadid = mDatabaseReference.push().getKey() ;
+
+                    //adding imge upload
+                    mRef.child(uid).setValue(model);
+
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    // hide progrees bar
+                    mbar.setVisibility(View.INVISIBLE);
+                    //  mprogressDialog.dismiss();
+                    Toast.makeText(getApplicationContext(), e.getMessage(),Toast.LENGTH_LONG).show();
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                 //   viewDialog.showDialog();
+                    mbar.setVisibility(View.VISIBLE);
+
+                    //      mprogressDialog.setTitle(" Uploading ...........");
+                }
+            });
+
+
+
+
+        }
+        else {
+            mbar.setVisibility(View.GONE);
+            Toast.makeText(getApplicationContext(), "Please Select image or add image Name ", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+
+        }
+
+    private void sentToHome() {
+        Intent intent = new Intent(create_Profile.this,HomePage.class);
+        startActivity(intent);
+        finish();
+
+    }
+/*
         String id = mRef.push().getKey();
 
         getProfile model = new getProfile(id, uid, name, email, gndr, bldGroup, numVisibility , "TRUE" , null);
@@ -467,7 +650,10 @@ public class create_Profile extends AppCompatActivity {
                 mbar.setVisibility(View.GONE);
             }
         });
-    }
+          }
+        */
+
+
 
 
     private void OpenDialogue() {
